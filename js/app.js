80e7460d0d5d40a57f3faac47b4d6cc6f3176644
Martin Zellner <martin.zellner@gmail.com>
2013-05-22 18:21:55 +0200

    Added Logfile, done some cleanup

--- js/app.js
+++ js/app.js
@@ -33,28 +33,44 @@ $(document).ready(function() {
       document.getElementById('page_count').textContent = pdfDoc.numPages;
     }
 
- 
+
  	var editor = ace.edit("editor");
     editor.setTheme("ace/theme/clouds");
     editor.getSession().setMode("ace/mode/latex");
-   
+
 
     // APP
 
+    // Paths
+    var rootPath = window.location.origin;
+    var apiPath = rootPath + '/api'
+
+    // Messages
+    var savedMessage = '<div class="alert alert-success">Saved.<a class="close" data-dismiss="alert" href="#">&times;</a></div>';
+    var compileSuccessMessage = '<div class="alert  alert-success">Successfully compiled<a class="close" data-dismiss="alert" href="#">&times;</a></div>';
+    var compilingMessage = '<div class="alert">Compiling...</div>';
+
+    // State
+    var currentProjectName = '';
+    var currentProjectHash = window.location.pathname.split('/')[window.location.pathname.split('/').length-2];
+    var currentFile = window.location.pathname.substring(window.location.pathname.lastIndexOf('/')+1);
+    var currentLog = '';
+
     function compileDocument(){
-    	var url = window.location.pathname;
-		var filename = url.substring(url.lastIndexOf('/')+1);
-		var content = editor.getValue(); 
-        $('.marketing').before('<div class="alert">Compiling...</div>').fadeIn(1000);
-		$.post('../api/compile', { filename: filename, content : content}, function(responseCode){
-			PDFJS.getDocument('../build/' + filename.slice(0, -4) + '.pdf').then(function getPdfHelloWorld(_pdfDoc) {
+    
+        $('.marketing').before(compilingMessage).fadeIn(1000);
+
+		$.post(apiPath + '/compile', { fileName: currentFile, projectHash : currentProjectHash,  content : editor.getValue()}, function(data){
+			var path = data.pdfUrl;
+			currentLog = data.compileLog;
+
+			PDFJS.getDocument(path).then(function getPdfHelloWorld(_pdfDoc) {
     			pdfDoc = _pdfDoc;
      			renderPage(pageNum);
-    		});
-			$('.alert').fadeOut();
-			$('.marketing').before('<div class="alert  alert-success">Successfully compiled<a class="close" data-dismiss="alert" href="#">&times;</a></div>').fadeIn(500);
-			$('.alert').fadeOut(5000);
-			
+     			$('.alert').fadeOut();
+				$('.marketing').before(compileSuccessMessage).fadeIn(500);
+				$('.alert').fadeOut(5000);
+    		});	
 		});
     }
 
@@ -72,16 +88,35 @@ $(document).ready(function() {
 	    renderPage(pageNum);
 	});
 
- 	// New Document
+	// New Document
     $("#newDocumentDialog").modal({
 		show: false
- 	}); 
+	});
+
+     $(".document-log").popover({
+     	placement: 'bottom',
+     	html: true,
+     	template: '<div class="popover popover-log"><div class="arrow"></div><div class="popover-inner  popover-log"><h3 class="popover-title popover-log"></h3><div class="popover-content popover-log"><p></p></div></div></div>',
+     	content: function(){
+     		var html = '<div id="logfile-container">' + currentLog.replace(/\n/g, '<br />') + "</div>";
+     		return html;
+
+     	}
+	});
+
 
     $("#newDocumentDialog .save-button").click(function(){
-		$.post('../api/newfile', { filename: $('#newDocumentDialog .filename').val()}, function(filename){
+
+	    currentProjectName = $('#newDocumentDialog .projectname').val();
+	    currentFile = $('#newDocumentDialog .filename').val();
+
+		$.post(apiPath + '/project/new', { projectName: currentProjectName, fileName: currentFile}, function(path){
+
 			editor.setValue("");
-			window.location.href = filename;
+			window.location.href = rootPath + '/projects/' + path;
+
 		});
+
 		$('#newDocumentDialog').modal('hide');
 	});
 
@@ -89,7 +124,7 @@ $(document).ready(function() {
 
      $("#settingsDialog").modal({
 		show: false
- 	}); 
+ 	});
 
     $("#settingsDialog .save-button").click(function(){
 		$('#settingsDialog').modal('hide');
@@ -97,13 +132,12 @@ $(document).ready(function() {
 
 
 	$('.document-save').click(function(){
-		var url = window.location.pathname;
-		var filename = url.substring(url.lastIndexOf('/')+1);
-		var content = editor.getValue(); 
-		$.post('../api/save', { filename: filename, content : content}, function(filename){
-           $('.marketing').before('<div class="alert alert-success">Saved.<a class="close" data-dismiss="alert" href="#">&times;</a></div>').fadeIn(500);
+
+		$.post(apiPath + '/save', { projectHash: currentProjectHash, fileName: currentFile, content : editor.getValue()}, function(filename){
+           $('.marketing').before(savedMessage).fadeIn(500);
            $('.alert').fadeOut(2000);
   		});
+
 	});
 
 	$('.document-compile').click(function(){
--- pywebtex.py
+++ pywebtex.py
@@ -2,57 +2,122 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.view import view_config
 from pyramid.response import Response
+from pyramid.response import FileResponse
+import subprocess
+
 import codecs
-import subprocess 
-import email.utils
+import os
+import hashlib
+import urlparse
+
+# Global variables
+
+workingDir = os.path.dirname(__file__)
+projectDir = 'projects'
+buildDir = 'build'
+
+# Init
+# Create project dir
+if not os.path.exists(os.path.join(workingDir, projectDir)): os.makedirs(os.path.join(workingDir, projectDir))
+
+
 
 @view_config(route_name='showDocument', renderer='pywebtex:templates/editor.pt')
 def showDocument(request):
-	docName =  request.matchdict['docName']
-	f = codecs.open('files/' + docName, 'r', "utf-8")
+	projectHash =  request.matchdict['projectHash']
+	fileName =  request.matchdict['fileName']
+
+	path = os.path.join(workingDir, projectDir, projectHash, fileName)
+
+	f = codecs.open(path, 'r', "utf-8")
 	content = f.read()
 	f.close()
-	return  {'filename' : docName, 'editorcontent' : content}
 
-@view_config(route_name='newFile')
+	return  {'filename' : fileName, 'editorcontent' : content}
+
+@view_config(route_name='new_project')
 def newFile(request):
-	fileName = request.POST.getone('filename')
-	f = codecs.open('files/' + fileName, 'w', "utf-8")
+	fileName = request.POST.getone('fileName')
+	projectName = request.POST.getone('projectName')
+	projectHash = hashlib.sha1(projectName).hexdigest()
+
+	print 'Creating project ' + projectName + ' in Folder ' + projectHash
+
+	# Creating new file
+	projectPath = os.path.join(workingDir, projectDir, projectHash)
+	if os.path.exists(projectPath):
+		print 'Error creating project. Folder already used.'
+		return Response('Error creating project. Folder already used.')
+	else:
+		os.makedirs(projectPath)
+
+	filePath = os.path.join(projectPath, fileName)
+	f = codecs.open(filePath, 'w', "utf-8")
 	f.close()
-	return Response(fileName)
+
+
+	projectUrl = './' + projectHash + '/' + fileName
+	return Response(projectUrl)
 
 @view_config(route_name='saveFile')
 def saveFile(request):
-	fileName = request.POST.getone('filename')
+
+	projectHash = request.POST.getone('projectHash')
+	fileName = request.POST.getone('fileName')
 	content = request.POST.getone('content')
-	f = codecs.open('files/' + fileName, 'r+', "utf-8")
+
+	path = os.path.join(workingDir, projectDir, projectHash, fileName)
+
+	f = codecs.open(path, 'r+', "utf-8")
 	f.write(content)
 	f.close()
+
 	return Response(fileName)
 
-@view_config(route_name='compile')
+@view_config(route_name='showpdf')
+def showpdf(request):
+	fileName = request.matchdict['fileName']
+	projectHash = request.matchdict['projectHash']
+
+	path = os.path.join(workingDir, projectDir, projectHash, 'build', fileName)
+	return FileResponse(path)
+
+@view_config(route_name='compile', renderer='json')
 def compile(request):
-	fileName = request.POST.getone('filename')
+	projectHash = request.POST.getone('projectHash')
+	fileName = request.POST.getone('fileName')
 	content = request.POST.getone('content')
-	f = codecs.open('files/' + fileName, 'r+', "utf-8")
+
+	latexWorkingDir = os.path.join(workingDir, projectDir, projectHash)
+	latexBuildDir = os.path.join(latexWorkingDir, buildDir)
+	latexFilePath = os.path.join(latexWorkingDir, fileName)
+
+	f = codecs.open(latexFilePath, 'r+', "utf-8")
 	f.write(content)
 	f.close()
-	errorCode = subprocess.Popen("latexmk  -p  -pdf -f -quiet -output-directory=../build/ " + fileName,  cwd='files/', shell=True)
+
+	compileArgs =["latexmk", "-latexoption=\"-interaction=nonstopmode\"", "-p", "-pdf", "-f", "-output-directory=./" + buildDir, fileName]
+	compileProcess = subprocess.Popen(compileArgs, cwd=latexWorkingDir,  stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
+	compileLog = compileProcess.communicate()[0]
+
+	cleanArgs = ["latexmk", "-latexoption=\"-interaction=nonstopmode\"", "-c", "-quiet", "-output-directory=./" + buildDir, fileName]
+	subprocess.Popen(cleanArgs,  cwd=latexWorkingDir, stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
+
 	
-	subprocess.Popen("latexmk -c -output-directory=../build/ " + fileName,  cwd='files/',  shell=True)
-	return Response(str(errorCode))
+	pdfUrl = './' + buildDir + '/' + fileName[:-4] + '.pdf'
+	return {'pdfUrl' : pdfUrl, 'compileLog': compileLog}
 
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route('showDocument', '/file/{docName}')
-    config.add_route('newFile', '/api/newfile')
+    config.add_route('showDocument', '/' + projectDir + '/{projectHash}/{fileName}')
+    config.add_route('new_project', '/api/project/new')
     config.add_route('saveFile', '/api/save')
     config.add_route('compile', '/api/compile')
     config.add_static_view(name='lib', path='pywebtex:lib')
     config.add_static_view(name='css', path='pywebtex:css')
     config.add_static_view(name='js', path='pywebtex:js')
-    config.add_static_view(name='build', path='pywebtex:build')
+    config.add_route('showpdf', '/' + projectDir + '/{projectHash}/' + buildDir + '/{fileName}')
     config.scan()
 
     app = config.make_wsgi_app()
